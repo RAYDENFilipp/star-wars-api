ae88a975e8a3c102d470818c9acf8346e8e121dd
Philipp Lypniakov <filipprayden@gmail.com>
2018-10-15 22:57:52 +0300

    Updates

---
+++ precache-manifest.7627d292f3b9467234026186d93f0ae1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb994a3747e9478b747c539daaae4775",
+    "url": "/star-wars-api/static/media/stars-background.bb994a37.png"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/star-wars-api/static/media/STARWARS.3f085be9.ttf"
+  },
+  {
+    "revision": "b5fd956b09ccf38a6a10",
+    "url": "/star-wars-api/static/js/runtime~main.b5fd956b.js"
+  },
+  {
+    "revision": "9df78c205c0c85a21af2",
+    "url": "/star-wars-api/static/js/main.9df78c20.chunk.js"
+  },
+  {
+    "revision": "b5cf4b6609ccffc7859d",
+    "url": "/star-wars-api/static/js/1.b5cf4b66.chunk.js"
+  },
+  {
+    "revision": "9df78c205c0c85a21af2",
+    "url": "/star-wars-api/static/css/main.de4d0171.chunk.css"
+  },
+  {
+    "revision": "b5cf4b6609ccffc7859d",
+    "url": "/star-wars-api/static/css/1.4e9f736b.chunk.css"
+  },
+  {
+    "revision": "5fe6c2c9f5b87c9f8702ba6ecec96c8c",
+    "url": "/star-wars-api/index.html"
+  }
+];
